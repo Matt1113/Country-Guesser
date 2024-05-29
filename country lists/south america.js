0a36c150c15ba3add589b666f182598d9cde1ab5
Matthew Baker <matthewbaker400@gmail.com>
2024-05-28 23:14:06 -0400

    south america added

---
+++ country lists/south america.js
@@ -0,0 +1,4 @@
+export const countries = [
+    "Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador",
+    "Guyana", "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela", "French Guiana"
+]
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const countryLists = {
     europe : '/country lists/europe.js',
-    us : '/country lists/us.js'
+    us : '/country lists/us.js',
+    'south america' : '/country lists/south america.js'
 }
 const svgMaps = {
     europe : '/svg maps/europe.svg',
-    us : '/svg maps/us.svg'
+    us : '/svg maps/us.svg',
+    'south america' : '/svg maps/south america.svg'
 }
 const countryElement = document.querySelector(".country")
 const incorrectElement = document.querySelector(".incorrect")
@@ -51,7 +53,7 @@ function displayRandomCountry() {
 function checkAnswer(event) {
     if (event.target.tagName === 'path') {
         //correct country selected
-        console.log(event.target.getAttribute('name'))
+        console.log(event.target.getAttribute('d'))
         if (event.target.getAttribute('name') === currentCountry) {
             updateAfterCorrectAnswer(event)
         } else { //incorrect country selected
